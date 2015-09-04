29a7ec2afde529fd429a40b15b2664c98a2a1227
Deep Thought <ti.42.plus@gmail.com>
2015-09-04 02:06:28 -0700

    added icon

--- lib/include.php
+++ lib/include.php
@@ -11,6 +11,7 @@ function print_head($title = NULL) {
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<link href="//fonts.googleapis.com/css?family=Roboto:400,700|Oswald|Advent+Pro&amp;subset=latin,greek" rel="stylesheet" type="text/css" />
 		<link href="/lib/styles.css" rel="stylesheet" type="text/css" />
+		<link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
 
 EOF;
 }
