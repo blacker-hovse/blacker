6e0827322f4a33edabf64304b4ea46aa761b60b6
Deep Toaster <ti.42.plus@gmail.com>
2016-01-09 03:37:52 -0800

    added blink

--- nearer/index.php
+++ nearer/index.php
@@ -1,3 +1,15 @@
 <?
 include(__DIR__ . '/../lib/include.php');
-?>
+?><!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+<?
+print_head('Nearer');
+?>	</head>
+	<body>
+		<div id="main">
+			<h1>Nearer</h1>
+			<h2>Play URL</h2>
+		</div>
+	</body>
+</html>
