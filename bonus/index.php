b45080a4b562adffcf658f315716a23ba54662da
Deep Toaster <ti.42.plus@gmail.com>
2016-05-13 00:05:33 -0700

    simplified bonus

--- bonus/index.php
+++ bonus/index.php
@@ -1,23 +1,3 @@
 <?
-include(__DIR__ . '/../lib/include.php');
-?><!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-<?
-print_head('Bonus');
-?>	</head>
-	<body>
-		<div id="main">
-			<h1>Bonus</h1>
-			<h2>You Win</h2>
-			<audio autoplay="autoplay" style="visibility: hidden;">
-				<source src="/lib/ride.mp3" type="audio/mpeg" />
-			</audio>
-		</div>
-<?
-print_footer(
-	'Copyright &copy; 2016 Will Yu',
-	'A service of Blacker House'
-);
-?>	</body>
-</html>
+header('Location: /lib/include.php');
+?>
